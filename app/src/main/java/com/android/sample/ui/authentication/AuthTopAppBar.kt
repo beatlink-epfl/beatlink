1ca61106e289d7277fe9e466801ad0ce7f3011b3
Luna Raithle <luna.raithle@epfl.ch>
2024-10-31 20:49:16 +0100

    Changed bottom nav icons to imageVectors and Change to the supporting text style

--- app/src/main/java/com/android/sample/ui/authentication/AuthTopAppBar.kt
+++ app/src/main/java/com/android/sample/ui/authentication/AuthTopAppBar.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import com.android.sample.ui.library.CornerIcons
+import com.android.sample.ui.components.CornerIcons
 import com.android.sample.ui.theme.PrimaryRed
 
 @OptIn(ExperimentalMaterial3Api::class)
