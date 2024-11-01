3df802e8490eb11ce143638e843962549b5228ae
Mikhail Perevoznyk <mikhail.perevoznyk@epfl.ch>
2024-11-01 04:42:03 +0100

    feat: add code for new screens
    -Made the needed changes for the new screens that were implemented

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -3,16 +3,10 @@ package com.android.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.navigation.compose.NavHost
@@ -26,12 +20,16 @@ import com.android.sample.ui.authentication.SignUpScreen
 import com.android.sample.ui.authentication.WelcomeScreen
 import com.android.sample.ui.library.LibraryScreen
 import com.android.sample.ui.map.MapScreen
-import com.android.sample.ui.navigation.BottomNavigationMenu
-import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Route
 import com.android.sample.ui.navigation.Screen
 import com.android.sample.ui.profile.ProfileScreen
+import com.android.sample.ui.search.DiscoverPeopleScreen
+import com.android.sample.ui.search.LiveMusicPartiesScreen
+import com.android.sample.ui.search.MostMatchedSongsScreen
+import com.android.sample.ui.search.SearchBarScreen
+import com.android.sample.ui.search.SearchScreen
+import com.android.sample.ui.search.TrendingSongsScreen
 import com.android.sample.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -73,6 +71,11 @@ fun BeatLinkApp() {
 
     navigation(startDestination = Screen.SEARCH, route = Route.SEARCH) {
       composable(Screen.SEARCH) { SearchScreen(navigationActions) }
+      composable(Screen.SEARCH_BAR) { SearchBarScreen(navigationActions) }
+      composable(Screen.TRENDING_SONGS) { TrendingSongsScreen(navigationActions) }
+      composable(Screen.MOST_MATCHED_SONGS) { MostMatchedSongsScreen(navigationActions) }
+      composable(Screen.LIVE_MUSIC_PARTIES) { LiveMusicPartiesScreen(navigationActions) }
+      composable(Screen.DISCOVER_PEOPLE) { DiscoverPeopleScreen(navigationActions) }
     }
 
     navigation(startDestination = Screen.LIBRARY, route = Route.LIBRARY) {
@@ -83,22 +86,4 @@ fun BeatLinkApp() {
       composable(Screen.PROFILE) { ProfileScreen(tmpUser, navigationActions) }
     }
   }
-}
-
-/** Temporary screen to test navigation */
-@Composable
-fun SearchScreen(navigationActions: NavigationActions) {
-  Scaffold(
-      modifier = Modifier.testTag("searchScreen"),
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = { route -> navigationActions.navigateTo(route) },
-            tabList = LIST_TOP_LEVEL_DESTINATION,
-            selectedItem = navigationActions.currentRoute())
-      },
-      content = { pd ->
-        Box(modifier = Modifier.padding(pd).fillMaxSize()) {
-          Text(text = "Search Screen", modifier = Modifier.align(Alignment.Center))
-        }
-      })
 }
