6a601f2859f8823963cd8bc6ce04db72cfc4a89e
Luna Raithle <luna.raithle@epfl.ch>
2024-11-01 08:58:30 +0100

    Optimize imports on the NavigationActions.kt file

--- app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/android/sample/ui/navigation/NavigationActions.kt
@@ -1,9 +1,6 @@
 package com.android.sample.ui.navigation
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
