2d69ea69504b1d19f81cbd5ef6e46efad1e9937a
Luna Raithle <109693331+lunasrc@users.noreply.github.com>
2024-11-04 19:44:05 +0100

    Update app/src/main/java/com/epfl/beatlink/MainActivity.kt
    Co-authored-by: Morgane Eléonore Magnin <93370883+memagnin@users.noreply.github.com>

--- app/src/main/java/com/epfl/beatlink/MainActivity.kt
+++ app/src/main/java/com/epfl/beatlink/MainActivity.kt
@@ -23,7 +23,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import com.android.sample.ui.library.LibraryScreen
+import com.epfl.beatlink.ui.library.LibraryScreen
 import com.epfl.beatlink.model.map.MapLocationRepository
 import com.epfl.beatlink.model.map.MapViewModel
 import com.epfl.beatlink.model.profile.ProfileViewModel
