f24a8bae61d5c8fae808a808fefa740bc3b427bb
Mikhail Perevoznyk <mikhail.perevoznyk@epfl.ch>
2024-11-04 16:59:41 +0100

    fix: duplicate lines

--- app/src/main/java/com/epfl/beatlink/ui/search/components/StandardSearchListings.kt
+++ app/src/main/java/com/epfl/beatlink/ui/search/components/StandardSearchListings.kt
@@ -1,12 +1,17 @@
 package com.epfl.beatlink.ui.search.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -60,5 +65,22 @@ fun <T> StandardLazyRow(
               .padding(horizontal = 21.dp)
               .padding(top = 15.dp, bottom = 15.dp)) {
         items(listOfItems.size) { index -> itemContent(listOfItems[index]) }
+      }
+}
+
+@Composable
+fun StandardFillerColumn(tag: String, paddingValues: PaddingValues) {
+  Column(
+      modifier =
+          Modifier.testTag(tag)
+              .fillMaxSize()
+              .padding(paddingValues)
+              .background(color = Color.White)) {
+        HorizontalDivider(
+            color = Color.LightGray, thickness = 1.dp, modifier = Modifier.testTag("divider"))
+
+        Spacer(modifier = Modifier.testTag("spacer").height(17.dp))
+
+        Text(text = "Not Drawn In Figma Yet", modifier = Modifier.testTag("placeholderText"))
       }
 }
