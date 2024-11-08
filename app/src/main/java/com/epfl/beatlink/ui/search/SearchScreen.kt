0aa45bc47cf481f97028c93d6e5ea095a00d3805
mikhailperevoznyk <62029066+mikhailperevoznyk@users.noreply.github.com>
2024-11-08 06:35:34 +0100

    Addition of a simple Search Page (#68)
    * feat: implement needed components for the designing of the search page
    
    -Cards.kt: Component used for the 3 different cards in the Search Page
    -SearchBars.kt: Component containing a Search Bar taking the whole width of the screen and another one with a back-arrow icon on the left
    -GradientTitle.kt: Component responsible for colouring the titles with the gradient colour, as well as treating the case if a title has an arrow next to it
    -ComposedIcons.kt: Component containing combined icons, now only having hte party icon for the PartyCard
    
    * feat: implement needed components for the designing of the search page
    
    -StandardSearchListings.kt: Component having LazyRows and LazyColumns used for listing cards for all screens
    
    * feat: add main search page screen with it's child screens
    
    -SearchScreen.kt: Main Search screen with hardcoded elements for visualising purposes
    -Only MostMatchedSongsScreen.kt and DiscoverPeopleScreen.kt were not implemented much, because of the missing of their architecture on the figma
    -Note: commited by mistake DiscoverPeopleScreen.kt with the other batch of files destined for components
    
    * feat: add test classes for new implementation
    
    * feat: add code for new screens
    
    -Made the needed changes for the new screens that were implemented
    
    * feat: add needed icons for new implementation
    
    * fix: format using ktfmtFormat
    
    * feat: add placeholder image for songs' cover
    
    * fix: fix assertion tests for CI
    
    * fix: fix assertion tests for CI v2
    
    * feat: add testTags for scaffolds of each screen of the additional screens of SearchScreen
    
    * feat: add tests for more coverage
    
    -Especially for SearchBarScreenTest.kt, added tests for correct behaviour of categories
    
    * feat: add scroll to for navigation tests on titles
    
    * fix: duplicate lines
    
    * fix: fix review comments

---
+++ app/src/main/java/com/epfl/beatlink/ui/search/SearchScreen.kt
@@ -0,0 +1,134 @@
+package com.epfl.beatlink.ui.search
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
+import com.epfl.beatlink.model.profile.ProfileData
+import com.epfl.beatlink.model.spotify.objects.SpotifyTrack
+import com.epfl.beatlink.model.spotify.objects.State
+import com.epfl.beatlink.ui.navigation.BottomNavigationMenu
+import com.epfl.beatlink.ui.navigation.LIST_TOP_LEVEL_DESTINATION
+import com.epfl.beatlink.ui.navigation.NavigationActions
+import com.epfl.beatlink.ui.navigation.Screen
+import com.epfl.beatlink.ui.search.components.FullSearchBar
+import com.epfl.beatlink.ui.search.components.GradientTitle
+import com.epfl.beatlink.ui.search.components.PartyCard
+import com.epfl.beatlink.ui.search.components.ProfileCard
+import com.epfl.beatlink.ui.search.components.SongCard
+import com.epfl.beatlink.ui.search.components.StandardLazyRow
+import com.epfl.beatlink.ui.theme.lightThemeBackground
+
+@Composable
+fun SearchScreen(navigationActions: NavigationActions) {
+  val song1 =
+      SpotifyTrack(
+          name = "Song1",
+          trackId = "1",
+          cover = "cover1",
+          duration = 120,
+          popularity = 80,
+          state = State.PAUSE)
+  val listOfTrendingSongs = listOf(song1)
+  val sortedByPopularitySongs = listOfTrendingSongs.sortedByDescending { it.popularity }
+
+  val song2 =
+      SpotifyTrack(
+          name = "Song2",
+          trackId = "1",
+          cover = "cover1",
+          duration = 120,
+          popularity = 80,
+          state = State.PAUSE)
+  val listOfTrendingSongs2 = listOf(song2)
+  val sortedByPopularitySongs2 = listOfTrendingSongs2.sortedByDescending { it.popularity }
+
+  val profile1 =
+      ProfileData(
+          username = "@username1", name = "Name1", bio = "bio1", links = 3, profilePicture = 1)
+  val listOfProfiles = listOf(profile1)
+
+  Scaffold(
+      topBar = { FullSearchBar(navigationActions) },
+      bottomBar = {
+        // Bottom navigation bar
+        BottomNavigationMenu(
+            onTabSelect = { route -> navigationActions.navigateTo(route) },
+            tabList = LIST_TOP_LEVEL_DESTINATION,
+            selectedItem = navigationActions.currentRoute())
+      },
+      modifier = Modifier.testTag("searchScreen")) { paddingValues ->
+        Column(
+            modifier =
+                Modifier.testTag("searchScreenColumn")
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .verticalScroll(rememberScrollState())
+                    .background(color = lightThemeBackground)) {
+              HorizontalDivider(
+                  color = Color.LightGray,
+                  thickness = 1.dp,
+                  modifier = Modifier.testTag("searchScreenDivider"))
+
+              Spacer(modifier = Modifier.testTag("searchScreenSpacer").height(16.dp))
+
+              StandardLazyRow(
+                  title = "TRENDING SONGS",
+                  listOfItems = sortedByPopularitySongs,
+                  itemContent = { song -> SongCard(song) },
+                  horizontalSpace = 25,
+                  navigationActions = navigationActions,
+                  screen = Screen.TRENDING_SONGS)
+
+              StandardLazyRow(
+                  title = "MOST MATCHED SONGS",
+                  listOfItems = sortedByPopularitySongs2,
+                  itemContent = { song -> SongCard(song) },
+                  horizontalSpace = 25,
+                  navigationActions = navigationActions,
+                  screen = Screen.MOST_MATCHED_SONGS)
+
+              GradientTitle("LIVE MUSIC PARTIES", true) {
+                navigationActions.navigateTo(Screen.LIVE_MUSIC_PARTIES)
+              }
+
+              LazyRow(
+                  horizontalArrangement = Arrangement.spacedBy(14.dp),
+                  modifier =
+                      Modifier.testTag("LIVE MUSIC PARTIESLazyColumn")
+                          .fillMaxWidth()
+                          .height(115.dp)
+                          .padding(horizontal = 21.dp)
+                          .padding(top = 15.dp, bottom = 15.dp)) {
+                    items(1) {
+                      PartyCard(
+                          title = "PARTY NAME",
+                          username = "@username",
+                          description = "Description, BLA BLA BLA")
+                    }
+                  }
+
+              StandardLazyRow(
+                  title = "DISCOVER PEOPLE",
+                  listOfItems = listOfProfiles,
+                  itemContent = { people -> ProfileCard(people) },
+                  horizontalSpace = 25,
+                  navigationActions = navigationActions,
+                  screen = Screen.DISCOVER_PEOPLE)
+            }
+      }
+}
