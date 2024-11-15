5402bf289f2e2211e2f4d7f888e4236f81dce508
Arthur Wolf <arthur.wlf@icloud.com>
2024-11-15 18:09:37 +0100

    Run KTFmt

--- app/src/androidTest/java/com/epfl/beatlink/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/epfl/beatlink/ui/map/MapScreenTest.kt
@@ -17,112 +17,111 @@ import org.junit.Test
 
 class MapScreenTest {
 
-    @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+  @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    @Test
-    fun mapScreen_displaysLoadingMapText_whenMapIsNotLoaded() {
-        // Create a new instance of MapViewModel for this test
-        val mapViewModel = MapViewModel(FakeMapLocationRepository()).apply {
-            isMapLoaded.value = false
-            permissionRequired.value = false
+  @Test
+  fun mapScreen_displaysLoadingMapText_whenMapIsNotLoaded() {
+    // Create a new instance of MapViewModel for this test
+    val mapViewModel =
+        MapViewModel(FakeMapLocationRepository()).apply {
+          isMapLoaded.value = false
+          permissionRequired.value = false
         }
 
-        composeTestRule.setContent {
-            MapScreen(
-                navigationActions = NavigationActions(rememberNavController()),
-                spotifyApiViewModel = null,
-                profileViewModel = viewModel(factory = ProfileViewModel.Factory),
-                mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
-                mapViewModel = mapViewModel
-            )
-        }
-
-        composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("MapScreenColumn").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("MapContainer").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
-
-        // Verify that "Loading map..." text is displayed when the map is not loaded
-        composeTestRule.onNodeWithText("Loading map...").assertIsDisplayed()
+    composeTestRule.setContent {
+      MapScreen(
+          navigationActions = NavigationActions(rememberNavController()),
+          spotifyApiViewModel = null,
+          profileViewModel = viewModel(factory = ProfileViewModel.Factory),
+          mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
+          mapViewModel = mapViewModel)
     }
 
-    @Test
-    fun mapScreen_displaysMap_whenMapIsLoaded() {
-        // Create a new instance of MapViewModel for this test
-        val mapViewModel = MapViewModel(FakeMapLocationRepository()).apply {
-            isMapLoaded.value = true
-            permissionRequired.value = false
+    composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("MapScreenColumn").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("MapContainer").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
+
+    // Verify that "Loading map..." text is displayed when the map is not loaded
+    composeTestRule.onNodeWithText("Loading map...").assertIsDisplayed()
+  }
+
+  @Test
+  fun mapScreen_displaysMap_whenMapIsLoaded() {
+    // Create a new instance of MapViewModel for this test
+    val mapViewModel =
+        MapViewModel(FakeMapLocationRepository()).apply {
+          isMapLoaded.value = true
+          permissionRequired.value = false
         }
 
-        composeTestRule.setContent {
-            MapScreen(
-                navigationActions = NavigationActions(rememberNavController()),
-                spotifyApiViewModel = null,
-                profileViewModel = viewModel(factory = ProfileViewModel.Factory),
-                mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
-                mapViewModel = mapViewModel
-            )
-        }
-
-        composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("MapScreenColumn").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("MapContainer").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
-
-        // Verify that the map is displayed when loaded
-        composeTestRule.onNodeWithTag("Map").assertIsDisplayed()
-        composeTestRule.onNodeWithTag("currentLocationFab").assertIsDisplayed()
+    composeTestRule.setContent {
+      MapScreen(
+          navigationActions = NavigationActions(rememberNavController()),
+          spotifyApiViewModel = null,
+          profileViewModel = viewModel(factory = ProfileViewModel.Factory),
+          mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
+          mapViewModel = mapViewModel)
     }
 
-    @Test
-    fun mapScreen_handlesPermissionResult_correctly() {
-        // Create a new instance of MapViewModel for this test
-        val mapViewModel = MapViewModel(FakeMapLocationRepository())
+    composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("MapScreenColumn").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("MapContainer").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
 
-        mapViewModel.permissionRequired.value = true
+    // Verify that the map is displayed when loaded
+    composeTestRule.onNodeWithTag("Map").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("currentLocationFab").assertIsDisplayed()
+  }
 
-        composeTestRule.setContent {
-            MapScreen(
-                navigationActions = NavigationActions(rememberNavController()),
-                spotifyApiViewModel = null,
-                profileViewModel = viewModel(factory = ProfileViewModel.Factory),
-                mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
-                mapViewModel = mapViewModel
-            )
-        }
+  @Test
+  fun mapScreen_handlesPermissionResult_correctly() {
+    // Create a new instance of MapViewModel for this test
+    val mapViewModel = MapViewModel(FakeMapLocationRepository())
 
-        // Simulate permissions granted
-        val permissions = mapOf(
+    mapViewModel.permissionRequired.value = true
+
+    composeTestRule.setContent {
+      MapScreen(
+          navigationActions = NavigationActions(rememberNavController()),
+          spotifyApiViewModel = null,
+          profileViewModel = viewModel(factory = ProfileViewModel.Factory),
+          mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
+          mapViewModel = mapViewModel)
+    }
+
+    // Simulate permissions granted
+    val permissions =
+        mapOf(
             Manifest.permission.ACCESS_FINE_LOCATION to true,
-            Manifest.permission.ACCESS_COARSE_LOCATION to true
-        )
-        val granted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
-                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
-        mapViewModel.onPermissionResult(granted)
-        mapViewModel.setLocationPermissionGranted(granted)
+            Manifest.permission.ACCESS_COARSE_LOCATION to true)
+    val granted =
+        permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+            permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+    mapViewModel.onPermissionResult(granted)
+    mapViewModel.setLocationPermissionGranted(granted)
 
-        // Verify that onPermissionResult updates permissionRequired and locationPermitted
-        assert(mapViewModel.locationPermitted.value)
-        assert(!mapViewModel.permissionRequired.value)
+    // Verify that onPermissionResult updates permissionRequired and locationPermitted
+    assert(mapViewModel.locationPermitted.value)
+    assert(!mapViewModel.permissionRequired.value)
+  }
+
+  @Test
+  fun mapScreen_requestsPermissions_whenPermissionRequired() {
+    // Create a new instance of MapViewModel for this test
+    val mapViewModel = MapViewModel(FakeMapLocationRepository())
+
+    composeTestRule.setContent {
+      MapScreen(
+          navigationActions = NavigationActions(rememberNavController()),
+          spotifyApiViewModel = null,
+          profileViewModel = viewModel(factory = ProfileViewModel.Factory),
+          mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
+          mapViewModel = mapViewModel)
     }
 
-    @Test
-    fun mapScreen_requestsPermissions_whenPermissionRequired() {
-        // Create a new instance of MapViewModel for this test
-        val mapViewModel = MapViewModel(FakeMapLocationRepository())
+    mapViewModel.permissionRequired.value = true
 
-        composeTestRule.setContent {
-            MapScreen(
-                navigationActions = NavigationActions(rememberNavController()),
-                spotifyApiViewModel = null,
-                profileViewModel = viewModel(factory = ProfileViewModel.Factory),
-                mapUsersViewModel = viewModel(factory = MapUsersViewModel.Factory),
-                mapViewModel = mapViewModel
-            )
-        }
-
-        mapViewModel.permissionRequired.value = true
-
-        assert(mapViewModel.permissionRequired.value) // No mocking required here
-    }
+    assert(mapViewModel.permissionRequired.value) // No mocking required here
+  }
 }
