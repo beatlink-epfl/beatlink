029ed55ae600bdd4d1a7ca08569c474d1f5c4d33
Mikhail Perevoznyk <mikhail.perevoznyk@epfl.ch>
2024-11-20 22:13:38 +0100

    feat: add new playlist data class for fetching user's spotify playlists
    -Added a new data class for playlists called UserPlaylist and not reusing the existing one that will be changed and used for collaborative playlists that will be implemented in another sprint

---
+++ app/src/main/java/com/epfl/beatlink/model/library/UserPlaylist.kt
@@ -0,0 +1,13 @@
+package com.epfl.beatlink.model.library
+
+import com.epfl.beatlink.model.spotify.objects.SpotifyTrack
+
+data class UserPlaylist(
+    val playlistID: String,
+    val ownerID: String,
+    val playlistCover: String, // TODO
+    val playlistName: String, // mandatory
+    val playlistPublic: Boolean = false,
+    val playlistSongs: List<SpotifyTrack>, // list of ids of SpotifyTrack
+    val nbTracks: Int
+)
