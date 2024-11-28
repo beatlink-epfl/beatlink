dbda38f70e5efd8af627f4220db2ca20860647cd
ericdall <eric.dallacqua@epfl.ch>
2024-11-28 13:05:33 +0100

    Test: update PlayScreenTest to reflect API integration adn parameters update.
    - Revised tests to align with the new `SpotifyApiViewModel` integration in `PlayScreen`.
    - Added validation for dynamic UI updates based on playback state.

--- app/src/androidTest/java/com/epfl/beatlink/ui/player/PlayScreenTest.kt
+++ app/src/androidTest/java/com/epfl/beatlink/ui/player/PlayScreenTest.kt
@@ -1,47 +1,55 @@
 package com.epfl.beatlink.ui.player
 
+import android.app.Application
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.epfl.beatlink.model.spotify.objects.SpotifyAlbum
-import com.epfl.beatlink.model.spotify.objects.SpotifyTrack
-import com.epfl.beatlink.model.spotify.objects.State
+import com.epfl.beatlink.repository.spotify.api.SpotifyApiRepository
 import com.epfl.beatlink.ui.navigation.NavigationActions
 import com.epfl.beatlink.ui.navigation.Route
+import com.epfl.beatlink.viewmodel.map.user.MapUsersViewModel
+import com.epfl.beatlink.viewmodel.spotify.api.SpotifyApiViewModel
+import org.json.JSONObject
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.stub
 
 @RunWith(AndroidJUnit4::class)
 class PlayScreenTest {
   private lateinit var navigationActions: NavigationActions
-  private val track = SpotifyTrack("track", "artist", "trackId", "cover", 100, 100, State.PLAY)
-  private val album =
-      SpotifyAlbum(
-          "spotifyId",
-          "name",
-          "cover",
-          "artist",
-          2024,
-          listOf(track),
-          10,
-          listOf("genre1", "genre2"),
-          100)
 
-  @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+  @Mock private lateinit var mockApplication: Application
+
+  @Mock private lateinit var mockApiRepository: SpotifyApiRepository
+
+  private lateinit var spotifyApiViewModel: SpotifyApiViewModel
 
   @Before
   fun setUp() {
+    MockitoAnnotations.openMocks(this)
     navigationActions = mock(NavigationActions::class.java)
     `when`(navigationActions.currentRoute()).thenReturn(Route.PROFILE)
+    spotifyApiViewModel = SpotifyApiViewModel(mockApplication, mockApiRepository)
+    mockApiRepository.stub { onBlocking { get("me/player") } doReturn Result.success(JSONObject()) }
     // Launch the composable under test
-    composeTestRule.setContent { PlayScreen(navigationActions, track, album) }
+    composeTestRule.setContent {
+      PlayScreen(
+          navigationActions, spotifyApiViewModel, viewModel(factory = MapUsersViewModel.Factory))
+    }
   }
 
   @Test
