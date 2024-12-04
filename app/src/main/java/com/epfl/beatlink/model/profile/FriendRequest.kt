200d79972bf91d8d81f1ed9d1c2749b430bf90dc
Luna Raithle <luna.raithle@epfl.ch>
2024-12-04 15:05:55 +0100

    Remove data class

--- app/src/main/java/com/epfl/beatlink/model/profile/FriendRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.epfl.beatlink.model.profile
-
-data class FriendRequest(
-    val ownRequests: Map<String, Boolean> = emptyMap(),
-    val friendRequests: Map<String, Boolean> = emptyMap(),
-    val allFriends: Map<String, FriendDetails> = emptyMap()
-)
-
-data class FriendDetails(val status: String)
