a5ad15d6be09599116ad08cf63a575f54c7380bd
Morgane Magnin <magmorgane@gmail.com>
2024-12-13 02:48:39 +0100

    test: add test for SongPreviewMapUsers

--- app/src/androidTest/java/com/epfl/beatlink/ui/map/SongPreviewMapUsersTest.kt
+++ app/src/androidTest/java/com/epfl/beatlink/ui/map/SongPreviewMapUsersTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.epfl.beatlink.R
 import com.epfl.beatlink.model.map.user.CurrentPlayingTrack
 import com.epfl.beatlink.model.map.user.Location
@@ -17,7 +18,11 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
 
 class SongPreviewMapUsersTest {
 
@@ -149,6 +154,21 @@ class SongPreviewMapUsersTest {
     composeTestRule.onNodeWithTag("artistName").assertIsDisplayed()
     composeTestRule.onNodeWithTag("albumName").assertIsDisplayed()
     composeTestRule.onNodeWithTag("username").assertIsDisplayed()
+  }
+
+  @Test
+  fun testClickOnUsername() {
+    composeTestRule.setContent {
+      SongPreviewMapUsers(
+          mapUser = testUser,
+          profileViewModel = profileViewModel,
+          navigationActions = navigationActions)
+    }
+    composeTestRule.onNodeWithTag("username").performClick()
+
+    `when`(profileViewModel.getUserIdByUsername(eq(testUser.username), any())).then { "testUserId" }
+
+    verify(profileViewModel).getUserIdByUsername(eq(testUser.username), any())
   }
 
   @Test
